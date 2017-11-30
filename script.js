e35a115ae0c7adc6c013861e22d97330461833d1
ulrika-mansson <32763965+ulrika-mansson@users.noreply.github.com>
2017-11-30 12:57:01 +0100

    test

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ var showProject = function() {
     // in every column add a div with class = title
     // in every title add the text and a issue creation function (issueModal)
     // the function should get a parameter (index of the column)
-    // appaned columns to the #main
+    // append columns to the #main
 
     var html = '';
     $('.columns').remove();
